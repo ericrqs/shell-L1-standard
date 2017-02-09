37d8911de79a21ffe7f292b933c70237b48f9806
ericrqs <eric.r@quali.com>
2017-02-09 18:25:40 -0500

    support CloudShell and TestShell server dirs

--- {{cookiecutter.project_slug}}/compile_driver.bat
+++ {{cookiecutter.project_slug}}/compile_driver.bat
@@ -4,8 +4,15 @@ taskkill /f /im {{cookiecutter.model_name}}.exe
 
 sleep 3
 
-copy dist\{{cookiecutter.model_name}}.exe                     "c:\Program Files (x86)\QualiSystems\CloudShell\Server\Drivers"
-copy {{cookiecutter.project_slug}}_runtime_configuration.json "c:\Program Files (x86)\QualiSystems\CloudShell\Server\Drivers"
+set driverdir="c:\Program Files (x86)\QualiSystems\CloudShell\Server\Drivers"
+IF EXIST %driverdir% GOTO :havecs
+set driverdir="c:\Program Files (x86)\QualiSystems\TestShell\Server\Drivers"
+:havecs
+
+copy dist\{{cookiecutter.model_name}}.exe                     %driverdir%
+copy {{cookiecutter.project_slug}}_runtime_configuration.json %driverdir%
+
+
 
 copy {{cookiecutter.project_slug}}_datamodel.xml               release\
 copy dist\{{cookiecutter.model_name}}.exe                      release\
